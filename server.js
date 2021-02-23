e78a9514f6927921ba73fb329959e2a51e5eb5de
weston-bailey <wdbailey87@gmail.com>
2021-02-23 13:34:50 -0800

    update packages

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
-require('dotenv').config();
-const express = require('express');
-const ejsLayouts = require('express-ejs-layouts');
-const app = express();
+// required packages
+const express = require('express')
+const ejsLayouts = require('express-ejs-layouts')
+const rowdy = require('rowdy-logger')
+
+// configure express app
+const app = express()
+const PORT = 3000
+// (╯°□°）╯︵ ┻━┻
+const rowdyResults = rowdy.begin(app)
 
 // Sets EJS as the view engine
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 // Specifies the location of the static assets folder
-app.use(express.static('static'));
-// Sets up body-parser for parsing form data
-app.use(express.urlencoded({ extended: false }));
+app.use(express.static('static'))
 // Enables EJS Layouts middleware
-app.use(ejsLayouts);
-
+app.use(ejsLayouts)
+// Sets up body-parser for parsing form data
+app.use(express.urlencoded({ extended: false }))
 // Adds some logging to each request
-app.use(require('morgan')('dev'));
+app.use(require('morgan')('dev'))
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
-});
+  res.send('Hello, backend!')
+})
 
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(PORT, () => {
+  console.log(`listening on PORT:${PORT}`)
+  rowdyResults.print()
+})
 
-// We can export this server to other servers like this
-module.exports = server;
